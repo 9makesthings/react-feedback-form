f6025fe918c2771f34091bc4f06b327286d1dfde
Nina Johnson <9makesthings@gmail.com>
2019-04-05 16:45:34 -0500

    set up routes on app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,19 +3,38 @@ import React, { Component } from 'react';
 import './App.css';
 import { HashRouter as Router, Route } from 'react-router-dom';
 
+// Route components:
+import Feeling from '../Feeling/Feeling';
+import Understand from '../Understand/Understand';
+import Supported from '../Supported/Supported';
+import Comments from '../Comments/Comments';
+
+// Static component:
+import ReviewFeedback from '../ReviewFeedback/ReviewFeedback';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4><i>Don't forget it!</i></h4>
+          </header>
+          <br/>
 
-        {/* routes will go here... */}
+          <div>
+            {/* routes will go here... */}
+            <Route exact path ="/" component={Feeling} />
+            <Route path="/understand" component={Understand} />
+            <Route path="/support" component={Supported} />
+            <Route path="/comments" component={Comments} />
+          </div>
 
-      </div>
+          <ReviewFeedback />
+
+        </div>
+      </Router>
     );
   }
 }
