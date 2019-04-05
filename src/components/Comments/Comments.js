1e564c5895547196faa05ddd8e9c734febd95b7b
Nina Johnson <9makesthings@gmail.com>
2019-04-05 18:01:48 -0500

    set up input for Comments, handleChange when typing

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -3,10 +3,24 @@ import { connect } from 'react-redux';
 
 class Comments extends Component {
 
+    state = {
+        comments: ''
+    }
+
+    handleChange = (event) => {
+        console.log( `in handleChange...`, event.target.value );
+        
+    }
+
     render() {
         return(
             <div>
                 <h2>Comments!</h2>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" 
+                            onChange={this.handleChange} />
+                </form>
             </div>
         );
     }
