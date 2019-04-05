d9e9a36262b7012c3d4c6c2aed7e63345f4c7141
Nina Johnson <9makesthings@gmail.com>
2019-04-05 17:22:12 -0500

    made form in Feeling and handleChange

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -3,10 +3,30 @@ import { connect } from 'react-redux';
 
 class Feeling extends Component {
 
+    state = {
+        feeling: 0
+    }
+
+    handleChange = (event) => {
+        console.log( `in handleChange...`, this.state );
+        
+        this.setState({
+            ...this.state,
+            feeling: event.target.value,
+        })
+    }
+
     render() {
         return(
             <div>
-                <h2>Feelings!</h2>
+                <h2>How are you feeling today?</h2>
+                <form onSubmit={this.handleSubmit} >
+                    <input type="number" name="feeling"
+                            min="1" max="5"
+                            onChange={this.handleChange} />
+
+                    <button type="submit">Next</button>
+                </form>
             </div>
         );
     }
