9917185dd8cfd83ff37f22eef7c1d76f0ded8a9a
Nina Johnson <9makesthings@gmail.com>
2019-04-07 19:02:34 -0500

    Admin displays feedback in table

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -9,6 +9,32 @@ class Admin extends Component {
         return(
             <div>
                 <h2>Admin stuff</h2>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Feeling</th>
+                            <th>Comprehension</th>
+                            <th>Support</th>
+                            <th>Comments</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {this.props.reduxState.storeFeedbackReducer.map( feedback => 
+                                <tr key={feedback.id}>
+                                    <td>{feedback.feeling}</td>
+                                    <td>{feedback.understanding}</td>
+                                    <td>{feedback.support}</td>
+                                    <td>{feedback.comments}</td>
+                                    <td>
+                                        <button>X</button>
+                                    </td>
+                                </tr>
+                            )}
+                    </tbody>
+                </table>
             </div>
         );
     }
